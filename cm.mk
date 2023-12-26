74907cc91dc2531dac8131b8b16f634f31643688
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2023-12-26 08:28:19 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-## Specify phone tech before including full_phone	
+#Specify phone tech before including full_phone	
 $(call inherit-product, vendor/cm/config/telephony.mk)
 
 # Release name
@@ -7,12 +7,10 @@ PRODUCT_RELEASE_NAME := a1000
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/lenovo/a1000/a1000-vendor-blobs.mk)
-
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/device_a1000.mk)
 
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a1000
 PRODUCT_NAME := cm_a1000
 PRODUCT_BRAND := lenovo
